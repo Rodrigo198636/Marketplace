d1687b1f9f178a59cb8f61742e9a5318d9dfeca4
Rodrigo Damian Pinea <rodri_p86@hotmail.com>
2024-09-21 09:43:45 -0300

    Update download.php

--- download.php
+++ download.php
@@ -37,7 +37,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'marketplace198638@gmail.com';                     //SMTP username
-    $mail->Password   = 'hwarjxorcijmejiv';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     /* $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; */            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
